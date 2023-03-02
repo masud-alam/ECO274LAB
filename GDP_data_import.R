8c4e3380287d04be326e19c83d5266602b0e95d7
Masud Alam <masud.sust@gmail.com>
2023-03-02 11:12:02 +0600

    Update GDP_data_import.R

--- GDP_data_import.R
+++ GDP_data_import.R
@@ -13,3 +13,24 @@ ggplot(dat, aes(year, NY.GDP.PCAP.KD, color=country)) + geom_line() +
 dat_SA <-  WDI(indicator='NY.GDP.PCAP.KD', country=c('NP','BD','PK', 'IN'), start=1960, end=2022)
 ggplot(dat_SA, aes(year, NY.GDP.PCAP.KD, color=country)) + geom_line() + 
   xlab('Year') + ylab('GDP per capita South Asia')
+
+
+
+###GDP_Growth_Bar Plot
+# Load required library
+library(ggplot2)
+
+# Create a sample data frame with GDP growth data
+year <- c(2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020)
+gdp_growth <- c(2.5, 2.8, 1.9, 2.5, 2.9, 3.1, 2.6, 2.2, 2.9, 2.3, -3.5)
+gdp_data <- data.frame(year, gdp_growth)
+
+# Create a bar plot of GDP growth over time
+ggplot(data = gdp_data, aes(x = year, y = gdp_growth)) +
+  geom_bar(stat = "identity", fill = "blue") +
+  geom_text(aes(label=gdp_growth), vjust=0)+
+  ggtitle("GDP Growth Over Time") +
+  xlab("Year") +
+  ylab("GDP Growth (%)")+
+  theme_bw()
+
